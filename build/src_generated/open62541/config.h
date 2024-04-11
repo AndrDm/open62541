9a52eaaf394823ae8c327db023f3f0f18237aa20
Andrey Dmitriev <58330296+AndrDm@users.noreply.github.com>
2024-04-11 06:53:09 +0200

    Missing files added

--- build/src_generated/open62541/config.h
+++ build/src_generated/open62541/config.h
@@ -11,9 +11,9 @@
 #define UA_OPEN62541_VER_MAJOR 1
 #define UA_OPEN62541_VER_MINOR 4
 #define UA_OPEN62541_VER_PATCH 0
-#define UA_OPEN62541_VER_LABEL "-rc2-378-g40ed38f94" /* Release candidate label, etc. */
-#define UA_OPEN62541_VER_COMMIT "v1.4.0-rc2-378-g40ed38f94"
-#define UA_OPEN62541_VERSION "v1.4.0-rc2-378-g40ed38f94"
+#define UA_OPEN62541_VER_LABEL "-rc2" /* Release candidate label, etc. */
+#define UA_OPEN62541_VER_COMMIT "unknown-commit"
+#define UA_OPEN62541_VERSION "v1.4.0-rc2"
 
 /**
  * Architecture
